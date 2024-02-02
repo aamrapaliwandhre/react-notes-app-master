a5c2eda3dca53f1dfc419820ccd52fbbfbc5f0b2
Aamrapali  Wandhre <rnwandhre12@gmail.com>
2024-02-01 19:32:34 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,16 +16,16 @@ const App = () => {
 			text: 'This is my second note!',
 			date: '21/04/2021',
 		},
-		{
-			id: nanoid(),
-			text: 'This is my third note!',
-			date: '28/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my new note!',
-			date: '30/04/2021',
-		},
+		// {
+		// 	id: nanoid(),
+		// 	text: 'This is my third note!',
+		// 	date: '28/04/2021',
+		// },
+		// {
+		// 	id: nanoid(),
+		// 	text: 'This is my new note!',
+		// 	date: '30/04/2021',
+		// },
 	]);
 
 	const [searchText, setSearchText] = useState('');
